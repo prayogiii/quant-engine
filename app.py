730af3c12939e08d950451598d1b8f93f15d5b36
prayogiii <akhdannprayogi@gmail.com>
2026-07-21 21:58:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1788,7 +1788,7 @@ else:
                     title=dict(text=chart_title, x=0.01, xanchor='left', font=dict(size=14, color='#e0e0e0')),
                     template="plotly_dark",
                     height=400,
-                    margin=dict(l=50, r=20, t=40, b=10),
+                    margin=dict(l=10, r=20, t=40, b=10),
                     dragmode='pan',
                     xaxis=dict(
                         title=None,
