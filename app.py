ea536f413ca30db3fa5a3cc3e6497a8d15d2f634
prayogiii <akhdannprayogi@gmail.com>
2026-07-29 19:08:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1108,7 +1108,7 @@ elif rrr >= 1.0:
     rrr_status = "Cukup (1.0 - 1.5) 🟡"
 else:
     rrr_status = "Buruk (< 1.0) 🔴"
-        # ============ PIVOT (ADAPTIF) ============
+# ============ PIVOT (ADAPTIF) ============
         if is_daytrade:
             today_jkt = datetime.now(pytz.timezone("Asia/Jakarta")).date()
             if not df_daily.empty:
