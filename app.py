38f736fd134f3aba72f4cc4de2e34d311c7b4ff8
prayogiii <akhdannprayogi@gmail.com>
2026-08-14 19:15:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -746,8 +746,7 @@ with st.sidebar:
         if st.button("🗑️ Reset Cache", use_container_width=True):
             st.cache_data.clear()
             st.success("Cache dibersihkan!")
-#==================== SCANNER SAHAM IDX ====================
-if scan_btn:
+    #==================== SCANNER SAHAM IDX ====================
     st.markdown("---")
     st.subheader("🔍 Scanner Saham IDX")
     mode_scan = st.selectbox(
